d042cf8fdf408438e2351b4db0e4f763ed6cb379
moritam <hayato.moritan@gmail.com>
2025-08-30 17:40:01 +0900

    change navigation toppage link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/">
                         <x-application-logo class="h-12 w-auto" />
                     </a>
                 </div>
