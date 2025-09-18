23808ea2c545fbe99e43c497bd9bfb33be3bcd64
Risa <chorubeem95@outlook.com>
2025-09-18 20:10:58 +0900

    make the create plan page

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Country;
-// use App\Models\Plan;
 use App\Models\TravelPlan;
 use App\Models\TravelStyle;
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,27 +14,31 @@
                     style="background-image: url('/images/airplane.png')">
                     <div class="grid grid-cols-2 grid-rows-2 h-full w-full gap-10 p-10">
                         <!--find others travel plans-->
-                        <a href="{{ route('plan.search') }}" class="w-full h-full relative bg-white/5 text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition px-2 py-3">
+                        <a href="{{ route('plan.search') }}"
+                            class="w-full h-full relative bg-white/5 text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition px-2 py-3">
                             <i class="fa-solid fa-hippo flex-none text-3xl m-2"></i>
                             <span class="leading-none">Find others<br>travel plans</span>
                         </a>
-                        <!--create your travel plan-->
-                        <a class="w-full h-full relative bg-white/5 text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition px-2 py-3">
+                        <!-- create your travel plan -->
+                        <a href="{{ route('plan.create') }}"
+                            class="w-full h-full relative bg-white/5 text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition px-2 py-3">
                             <i class="fa-solid fa-plane flex-none text-3xl m-2"></i>
                             <span class="leading-none">Create your<br>travel plan</span>
                         </a>
                         <!--profile-->
-                        <a class="w-full h-full relative bg-white/5  text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition">
+                        <a
+                            class="w-full h-full relative bg-white/5  text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition">
                             <i class="fa-solid fa-user text-3xl m-2"></i>
                             <span>Profile</span>
                         </a>
                         <!--chat-->
-                        <a class="w-full h-full relative bg-white/5  text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition">
+                        <a
+                            class="w-full h-full relative bg-white/5  text-white font-bold rounded-lg flex items-center justify-center text-4xl hover:bg-white/40 border border-transparent hover:border-orange-500 transition">
                             <i class="fa-solid fa-comments flex-none text-3xl m-2"></i>
                             <span class="">Chat page</span>
                         </a>
                     </div>
-                </div>   
+                </div>
             </div>
         </div>
     </div>
