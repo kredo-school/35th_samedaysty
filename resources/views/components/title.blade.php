4112f95354e3885712c9ef29dbf6b4d7feb0df54
rin-taro013 <rinta317871120@outlook.jp>
2025-10-04 17:48:28 +0900

    added short-cut to plans in dashboard

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,4 +1,5 @@
 <h1 class="text-3xl md:text-4xl me-auto ps-10 pt-5 text-gray-900 dark:text-gray-100">{{ $slot }}</h1>
 <div
     class="bg-gradient-to-r from-orange-500 to-white via-white dark:from-orange-400 dark:to-gray-800 dark:via-gray-700 h-[2px]">
-    &nbsp;</div>
+    &nbsp;
+</div>
